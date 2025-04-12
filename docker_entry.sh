949b8cb5cadd5b5d01d7f9dba6182be1bc4b6f98
NinePiece2 <romit.sagu@hotmail.com>
2025-04-12 01:49:17 -0400

    Update

--- docker_entry.sh
+++ docker_entry.sh
@@ -3,4 +3,4 @@
 ${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${GAMEDIR} +login anonymous \
      +app_update ${GAMEID} validate +quit
 
-exec ${GAMEDIR}/LocalAdmin ${SERVERPORT} --acceptEULA --useDefault
+exec ${GAMEDIR}/LocalAdmin ${SERVERPORT} --acceptEULA --useDefault --printStd
