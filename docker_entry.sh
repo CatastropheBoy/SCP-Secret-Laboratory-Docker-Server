8ec5b0e765152c96a453d7769d1f82aac12b28a3
NinePiece2 <romit.sagu@hotmail.com>
2025-04-12 03:51:27 -0400

    Update

--- docker_entry.sh
+++ docker_entry.sh
@@ -3,7 +3,10 @@
 ${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${GAMEDIR} +login anonymous \
      +app_update ${GAMEID} validate +quit
 
-cd ${GAMEDIR}
+cd "${GAMEDIR}"
+
+# Ensure config directory exists
+mkdir -p "${CONFIGDIR}"
 
 # Edit the config file
 set_config_value() {
@@ -11,7 +14,7 @@ set_config_value() {
     local key="$2"
     local value="$3"
 
-    if grep -q "^${key}:" "$file"; then
+    if grep -q "^${key}:" "$file" 2>/dev/null; then
         sed -i "s|^${key}:.*|${key}: ${value}|" "$file"
     else
         echo "${key}: ${value}" >> "$file"
@@ -19,11 +22,10 @@ set_config_value() {
 }
 
 echo "Editing config file \"${CONFIGDIR}/config_gameplay.txt\""
-cat "${CONFIGDIR}/config_gameplay.txt"
 
-set_config_value "\"${CONFIGDIR}/config_gameplay.txt\"" "server_name" "${SERVERNAME}"
-set_config_value "\"${CONFIGDIR}/config_gameplay.txt\"" "minimum_players" "${MINPLAYERS}"
+set_config_value "${CONFIGDIR}/config_gameplay.txt" "server_name" "${SERVERNAME}"
+set_config_value "${CONFIGDIR}/config_gameplay.txt" "minimum_players" "${MINPLAYERS}"
+
+cat "${CONFIGDIR}/config_gameplay.txt"
 
 exec ./LocalAdmin ${SERVERPORT} --acceptEULA --useDefault
-
-cat "${CONFIGDIR}/config_gameplay.txt"
